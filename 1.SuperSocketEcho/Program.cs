2a9f2d32f979c756f04af0ddf84c64c46539e246
Hotkey <rhanwnwmf@protonmail.ch>
2019-09-01 18:10:52 +0900

    초기 커밋.

---
+++ 1.SuperSocketEcho/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.Common;
+using SuperSocket.Facility.Protocol;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
+
+namespace _1.SuperSocketEcho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyAppServer server = new MyAppServer();
+
+            server.Setup(new RootConfig(),new ServerConfig()
+            {
+                Port = 3000,
+                Ip = "Any",
+            });
+
+            server.Start();
+
+            while (Console.ReadLine() != "q")
+            {
+
+            }
+        }
+    }
+
+    public class MyReceiveFilter : FixedHeaderReceiveFilter<BinaryRequestInfo>
+    {
+        public MyReceiveFilter()
+            : base(6)
+        {
+
+        }
+
+        protected override int GetBodyLengthFromHeader(byte[] header, int offset, int length)
+        {
+            return (int)header[offset + 4] + (int)header[offset + 5] * 256 - 6;
+        }
+
+        protected override BinaryRequestInfo ResolveRequestInfo(ArraySegment<byte> header, byte[] bodyBuffer, int offset, int length)
+        {
+            var byteTmp = bodyBuffer.CloneRange(offset, length);
+
+            return new BinaryRequestInfo(BitConverter.ToUInt32(header.Array, 0).ToString(), MyReceiveFilter.Combine(header.Array, byteTmp));
+        }
+
+        public static byte[] Combine(byte[] first, byte[] second)
+        {
+            byte[] ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            return ret;
+        }
+    }
+
+    public class MyAppServer : AppServer<MyAppSession, BinaryRequestInfo>
+    {
+        public MyAppServer() : base(new DefaultReceiveFilterFactory<MyReceiveFilter, BinaryRequestInfo>())
+        {
+            this.NewSessionConnected += new SessionHandler<MyAppSession>(MyServer_NewSessionConnected);
+            this.SessionClosed += new SessionHandler<MyAppSession, CloseReason>(MyServer_SessionClosed);
+            this.NewRequestReceived += new RequestHandler<MyAppSession, BinaryRequestInfo>(MyServer_NewRequestReceived);
+        }
+
+        private void MyServer_NewRequestReceived(MyAppSession session, BinaryRequestInfo requestinfo)
+        {
+            Console.WriteLine("MyServer_NewRequestReceived");
+
+            session.Send(requestinfo.Body,0, requestinfo.Body.Length);
+        }
+
+        private void MyServer_SessionClosed(MyAppSession session, CloseReason value)
+        {
+            Console.WriteLine("MyServer_UserClosed");
+        }
+
+        private void MyServer_NewSessionConnected(MyAppSession session)
+        {
+            Console.WriteLine("MyServer_NewUserConnected");
+        }
+    }
+
+    public class MyAppSession : AppSession<MyAppSession, BinaryRequestInfo>
+    {
+
+    }
+}
---
+++ 1.SuperSocketEchoClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SuperSocket.ClientEngine;
+
+namespace _1.SuperSocketEchoClient
+{
+    class Program
+    {
+        private static AsyncTcpSession tcpSession;
+        static void Main(string[] args)
+        {
+            tcpSession = new AsyncTcpSession();
+
+            tcpSession.Connected += tcpSession_Connected;
+            tcpSession.Closed += tcpSession_Closed;
+            tcpSession.DataReceived += tcpSession_DataReceived;
+            tcpSession.Error += tcpSession_Error;
+            tcpSession.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000));
+            var tmpData = "";
+            while (tmpData != "q")
+            {
+                tmpData = Console.ReadLine();
+
+                var buffer = new CGD.buffer(50);
+
+                buffer.append<int>(123);
+                buffer.append<short>(0);
+                buffer.append<string>(tmpData);
+                buffer.set_front<short>(buffer.Count, 4);
+
+                tcpSession.Send(buffer);
+            }
+        }
+
+        private static void tcpSession_Error(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine("tcpSession_Error");
+        }
+
+        private static void tcpSession_DataReceived(object sender, DataEventArgs e)
+        {
+            AsyncTcpSession session = sender as AsyncTcpSession;
+
+            byte[] tmpBuffer = e.Data;
+            var buffer = new CGD.buffer(e.Data, 0, e.Length);
+
+            int bufferType = (int)buffer.extract_uint();
+            ushort bufferLength  = (ushort)buffer.extract_short();
+            string bufferData = buffer.extract_string();
+                
+            Console.WriteLine("tcpSession_DataReceived");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Type   : " + bufferType);
+            Console.WriteLine("Length : " + bufferLength);
+            Console.WriteLine("Data   : " + bufferData);
+            Console.WriteLine("---------------------------------");
+        }
+
+        private static void tcpSession_Closed(object sender, EventArgs e)
+        {
+            Console.WriteLine("tcpSession_Closed");
+        }
+
+        private static void tcpSession_Connected(object sender, EventArgs e)
+        {
+            AsyncTcpSession session = sender as AsyncTcpSession;
+
+            Console.WriteLine("tcpSession_Connected");
+
+            var buffer = new CGD.buffer(50);
+
+            buffer.append<int>(123);
+            buffer.append<short>(0);
+            buffer.append<string>("전송한 데이터");
+            buffer.set_front<short>(buffer.Count, 4);
+
+            session.Send(buffer);
+        }
+    }
+}
